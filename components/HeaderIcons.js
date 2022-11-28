613831581a8fbe85b38d54f122f78a38f8fffe30
paixaoariellll <paixaoariellll@gmail.com>
2022-11-28 13:02:18 -0300

    next-auth + mui

--- components/HeaderIcons.js
+++ components/HeaderIcons.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function HeaderIcons({ Icon, active }) {
     return (
-        <div className='flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-red-600 group'>
-            <Icon className={`h - 5 mx-auto sm:h-7 text-center text-gray-500 group-hover:text-red-600
+        <div className='flex items-center cursor-pointer md:px-10 sm:h-10 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-red-600 group'>
+            <Icon className={`h-5 mx-auto sm:h-7 text-center text-gray-500 group-hover:text-red-600
             ${active && "text-red-600"}
             `}
             />
